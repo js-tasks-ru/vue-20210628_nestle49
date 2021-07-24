9e74b747546e4b83f9c81d8c09c3f13610d99872
Albert <nestle49@yandex.ru>
2021-07-24 20:22:07 +0200

    n

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,29 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const vueCalculator = defineComponent({
+  data() {
+    return {
+      firstTerm: 0,
+      secondTerm: 0,
+      operation: 'sum',
+    }
+  },
+  computed: {
+    result(){
+      switch (this.operation) {
+        case 'sum':
+          return this.firstTerm + this.secondTerm
+        case 'subtract':
+          return this.firstTerm - this.secondTerm
+        case 'multiply':
+          return this.firstTerm * this.secondTerm
+        case 'divide':
+          return this.firstTerm / this.secondTerm
+        default:
+          return 0
+      }
+    },
+  },
+});
+
+createApp(vueCalculator).mount('#app');
